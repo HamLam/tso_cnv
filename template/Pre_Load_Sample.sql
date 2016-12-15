1c315bbe92cd72b810c0d05a3ea30805e9bb77eb
HamLam <lamx0031@umn.edu>
2016-12-15 14:47:29 -0600

    Create Pre_Load_Sample.sql

---
+++ template/Pre_Load_Sample.sql
@@ -0,0 +1,22 @@
+
+CREATE TABLE `cnv_sample_name_bwa_pileup_no_dup`(
+chr VARCHAR(32),
+pos INT,
+coverage INT,
+mapped_reads INT
+);
+
+
+CREATE TABLE `cnv_sample_name_bwa_pileup`(
+chr VARCHAR(32),
+pos INT,
+coverage INT
+);
+
+
+
+CREATE TABLE `cnv_sample_name_bowtie_pileup`(
+chr VARCHAR(32),
+pos INT,
+coverage INT
+);
